13cf66216c741f8397f6391171323476e7dc4326
Tom-Niesytto <tom_niesytto@yahoo.com>
2020-04-01 22:16:51 -0500

    1. added hello_world and test_file_exists 2. modify Video2_5_GUI_tkinter_another_button to delete

--- Section1/Video1_4_First_GUI_tkinter_icon.py
+++ Section1/Video1_4_First_GUI_tkinter_icon.py
@@ -3,12 +3,8 @@ Created on Feb 24, 2019
 @author: Burkhard A. Meier
 '''
 
-
-
-
-
-
 import tkinter as tk        # alias as tk
+import sys
 
 gui = tk.Tk()                           # create class instance
 gui.geometry('600x400')                 # specify a window width and height
---
+++ Section1/test_file_exists.py
@@ -0,0 +1,7 @@
+import os.path
+from os import path
+
+if(path.exists("py.ico")):
+    print('Yes')
+else:
+    print('No')
